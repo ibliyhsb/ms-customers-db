b6759f69096f422a6ec5f53bb041ee91190c434c
Alemstar <al.faguas@duocuc.cl>
2025-12-08 03:11:45 -0300

    swagger config

--- src/main/java/cl/duoc/ms_customers_db/config/SwaggerConfig.java
+++ src/main/java/cl/duoc/ms_customers_db/config/SwaggerConfig.java
@@ -1,9 +1,18 @@
 package cl.duoc.ms_customers_db.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfig {
-    // Springdoc OpenAPI configurará automáticamente Swagger
-    // Accede a: http://localhost:8080/swagger-ui.html
+    @Bean
+    public OpenAPI apiInfo(){
+        return new OpenAPI() 
+                   .info(new Info().title("Customers microservice - Database (DB) Documentation.")
+                                    .description("This microservice is responsible for client data access and management in the database.")
+                                    .version("1.0"));
+    }
 }
